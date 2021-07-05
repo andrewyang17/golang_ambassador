6e674544d058e009ebca6857cc98ab3a0f0c7c83
andrewyang17 <50484992+andrewyang17@users.noreply.github.com>
2021-07-05 10:18:53 -0500

    scope paylod JWT

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -4,12 +4,21 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"strconv"
+	"strings"
+	"time"
 )
+
+const secretKey = "secret"
+
+type ClaimsWithScope struct {
+	jwt.StandardClaims
+	Scope string
+}
 
 func IsAuthenticated(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte("secret"), nil
+	token, err := jwt.ParseWithClaims(cookie, &ClaimsWithScope{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secretKey), nil
 	})
 	if err != nil || !token.Valid {
 		c.Status(fiber.StatusUnauthorized)
@@ -17,19 +26,39 @@ func IsAuthenticated(c *fiber.Ctx) error {
 			"message": "unauthenticated",
 		})
 	}
+
+	payload := token.Claims.(*ClaimsWithScope)
+	isAmbassador := strings.Contains(c.Path(), "/api/ambassador")
+
+	if (payload.Scope == "admin" && isAmbassador) || (payload.Scope == "ambassador" && !isAmbassador) {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
 	return c.Next()
+}
+
+func GenerateJWT(id uint, scope string) (string, error){
+	payload := ClaimsWithScope{}
+	payload.Subject = strconv.Itoa(int(id))
+	payload.ExpiresAt = time.Now().Add(time.Hour * 24).Unix()
+	payload.Scope = scope
+
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte(secretKey))
 }
 
 func GetUserId(c *fiber.Ctx) (uint, error) {
 	cookie := c.Cookies("jwt")
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte("secret"), nil
+	token, err := jwt.ParseWithClaims(cookie, &ClaimsWithScope{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secretKey), nil
 	})
 	if err != nil {
 		return 0, err
 	}
 
-	payload := token.Claims.(*jwt.StandardClaims)
+	payload := token.Claims.(*ClaimsWithScope)
 	id, _ := strconv.Atoi(payload.Subject)
 
 	return uint(id), nil
